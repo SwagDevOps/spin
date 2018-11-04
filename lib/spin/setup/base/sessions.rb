7351aebbe872355ff4edb0c6075986ec38e7a8e0
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-11-04 17:34:05 +0100

    base/sessions (setup) environment use

--- lib/spin/setup/base/sessions.rb
+++ lib/spin/setup/base/sessions.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 enable(:sessions)
+
 {
-  key: 'unicorn',
-  path: '/',
-  expire_after: 2_592_000,
-  secret: 'change_me',
-  old_secret: 'also_change_me',
-}.tap { |settings| set(:sessions, settings) }
+  key: ENV.fetch('SESSION_KEY', 'unicorn.session'),
+  path: ENV.fetch('SESSION_PATH', '/'),
+  domain: ENV['SESSION_DOMAIN'],
+  expire_after: ENV.fetch('SESSION_EXPIRE_AFTER', 2_592_000).to_i,
+  secret: ENV.fetch('SESSION_SECRET'),
+  old_secret: ENV['SESSION_OLD_SECRET'],
+}.delete_if { |k, v| v.to_s.empty? }.tap do |settings|
+  set(:sessions, settings)
+end
