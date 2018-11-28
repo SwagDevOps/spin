458fd1b25ee71d888a71c99411457eeb29514874
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-11-28 19:19:08 +0100

    controller/base (dev) introduced

---
+++ dev/web_app/controller/base.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../controller'
+
+# Base controller
+class WebApp::Controller::Base < WebApp::Base
+  def initialize(*args)
+    super
+  end
+end
