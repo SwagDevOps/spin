c143ac738c67cfb563672c4455fe78fe4d0a9d0c
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-12-09 18:54:53 +0100

    controller/home (dev) fix inheritance + minor changes

--- dev/web_app/controller/home.rb
+++ dev/web_app/controller/home.rb
@@ -4,8 +4,6 @@ require 'spin/controller/base'
 require_relative '../controller'
 
 # Home controller
-class WebApp::Controller::Home < WebApp::Base
-  get '/' do
-    erb(:welcome)
-  end
+class WebApp::Controller::Home < WebApp::Controller::Base
+  get('/') { erb(:welcome) }
 end
