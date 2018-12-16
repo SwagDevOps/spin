95dafc4325734165cc79fc778f066c9e89293acc
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-12-16 18:41:01 +0100

    spin_spec (spec) introduced

---
+++ spec/spin_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+describe Spin, :spin do
+  it { expect(described_class).to be_const_defined(:VERSION) }
+  it { expect(described_class).to be_const_defined(:Autoloadable) }
+  it { expect(described_class).to be_const_defined(:Base) }
+  it { expect(described_class).to be_const_defined(:Config) }
+  it { expect(described_class).to be_const_defined(:Container) }
+  it { expect(described_class).to be_const_defined(:Controller) }
+  it { expect(described_class).to be_const_defined(:Helpers) }
+  it { expect(described_class).to be_const_defined(:Initializer) }
+  it { expect(described_class).to be_const_defined(:Setup) }
+  it { expect(described_class).to be_const_defined(:User) }
+end
