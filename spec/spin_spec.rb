1601cdf516362189e0bb793798758e174ca645e9
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-12-17 01:55:50 +0100

    spin_spec (spec) examples added

--- spec/spin_spec.rb
+++ spec/spin_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# constants
+# constants ---------------------------------------------------------
 describe Spin, :spin do
   it { expect(described_class).to be_const_defined(:VERSION) }
   it { expect(described_class).to be_const_defined(:Autoloadable) }
@@ -15,6 +15,31 @@ describe Spin, :spin do
   it { expect(described_class).to be_const_defined(:User) }
 end
 
+# class methods -----------------------------------------------------
+describe Spin, :spin do
+  context '.constants' do
+    let(:expected) do
+      # @formatter:off
+      [:Container,
+       :Pathname,
+       :Controller,
+       :Helpers,
+       :Initializer,
+       :Setup,
+       :User,
+       :VERSION,
+       :Dotenv,
+       :Autoloadable,
+       :Base,
+       :Config,
+       :DI].sort
+      # @formatter:on
+    end
+
+    it { expect(described_class.constants.sort).to eq(expected) }
+  end
+end
+
 # @see https://www.rubydoc.info/github/dry-rb/dry-auto_inject/master/Dry/AutoInject/Builder
 describe Class, :'spin/di' do
   let(:subject) { Spin.const_get(:DI) }
@@ -24,11 +49,15 @@ describe Class, :'spin/di' do
   it { expect(subject).to respond_to(:'[]') }
 end
 
-# instance methods
+# instance methods --------------------------------------------------
 describe Spin, :spin do
   let(:subject) do
     silence_stream($stdout) { described_class.new }
   end
 
   it { expect(subject).to respond_to(:container) }
+
+  # missing methods
+  it { expect(subject).to respond_to(:paths) }
+  it { expect(subject).to respond_to(:controller_class) }
 end
