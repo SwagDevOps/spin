d1c9bdb86a765bd339ba3ddb7807666330daf386
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-12-19 08:58:26 +0100

    config_reader_spec (spec) examples added for sample ``app.yml``

--- spec/spin/config_reader_spec.rb
+++ spec/spin/config_reader_spec.rb
@@ -5,3 +5,37 @@ describe Spin::ConfigReader, :'spin/config_reader' do
   it { expect(described_class).to be_const_defined(:Cache) }
   it { expect(described_class).to be_const_defined(:Loader) }
 end
+
+describe Spin::ConfigReader, :'spin/config_reader' do
+  let(:paths) { [SAMPLES_PATH.join('config')] }
+  let(:subject) { described_class.new(paths) }
+
+  it { expect(subject).to respond_to(:get) }
+
+  context '.get' do
+    it { expect(subject.get('app')).to be_frozen }
+    it { expect(subject.get('app')).to be_a(OpenStruct) }
+
+    it { expect(subject.get('app.math')).to be_frozen }
+    it { expect(subject.get('app.math')).to be_a(OpenStruct) }
+    it { expect(subject.get('app.math.PI')).to eq(3.14159) }
+
+    it { expect(subject.get('app.heroes')).to be_frozen }
+    it { expect(subject.get('app.heroes')).to be_a(Array) }
+  end
+
+  context '.to_a' do
+    it { expect(subject.to_a).to eq(paths) }
+  end
+end
+
+describe Spin::ConfigReader, :'spin/config_reader' do
+  let(:paths) { [SAMPLES_PATH.join('config')] }
+  let(:subject) { described_class.new(paths) }
+
+  let(:tested) { subject.get('app.heroes').map(&:frozen?).uniq }
+
+  context '.get().map(&:frozen?).uniq' do
+    it { expect(tested).to eq([true]) }
+  end
+end
