435e53f17076a3f329a39aee28868f012bd54e37
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-12-22 15:46:28 +0100

    config_reader_spec (spec) WIP

--- spec/spin/config_reader_spec.rb
+++ spec/spin/config_reader_spec.rb
@@ -23,12 +23,33 @@ describe Spin::ConfigReader, :'spin/config_reader' do
     it { expect(subject.get('app.heroes')).to be_frozen }
     it { expect(subject.get('app.heroes')).to be_a(Array) }
   end
+end
+
+# paths should include ``APP_ENV`` ----------------------------------
+describe Spin::ConfigReader, :'spin/config_reader' do
+  let(:paths) { [SAMPLES_PATH.join('config')] }
+  let(:subject) { described_class.new(paths) }
 
   context '.to_a' do
-    it { expect(subject.to_a).to eq(paths) }
+    let(:app_env) { 'test' }
+    let(:expected_paths) do
+      # @formatter:off
+      [
+        SAMPLES_PATH.join('config', app_env),
+        SAMPLES_PATH.join('config')
+      ]
+      # @formatter:on
+    end
+
+    it do
+      with_env('APP_ENV' => app_env) do
+        expect(subject.to_a).to eq(expected_paths)
+      end
+    end
   end
 end
 
+# all values should be frozen ---------------------------------------
 describe Spin::ConfigReader, :'spin/config_reader' do
   let(:paths) { [SAMPLES_PATH.join('config')] }
   let(:subject) { described_class.new(paths) }
@@ -37,5 +58,23 @@ describe Spin::ConfigReader, :'spin/config_reader' do
 
   context '.get().map(&:frozen?).uniq' do
     it { expect(tested).to eq([true]) }
+  end
+end
+
+# merging files by env ----------------------------------------------
+describe Spin::ConfigReader, :'spin/config_reader', :wip do
+  let(:paths) { [SAMPLES_PATH.join('config')] }
+  let(:app_env) { 'test' }
+  let(:subject) do
+    with_env('APP_ENV' => app_env) do
+      described_class.new(paths)
+    end
+  end
+
+  it { expect(subject).to respond_to(:get) }
+
+  context '.get' do
+    it { expect(subject.get('app.version')).to be_a(OpenStruct) }
+    it { expect(subject.get('app.math')).to be_a(OpenStruct) }
   end
 end
