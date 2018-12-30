8741e58b927510027cbe214e50b9155d16a78837
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-12-30 17:42:33 +0100

    cache (lib) introduced

---
+++ lib/spin/cache.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+# Copyright (C) 2017-2018 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
+require_relative '../spin'
+require 'forwardable'
+
+# Cache
+class Spin::Cache
+  autoload(:Moneta, 'moneta')
+
+  attr_reader :storage
+
+  extend Forwardable
+
+  def_delegators(:storage,
+                 :[]=,
+                 :clear,
+                 :close,
+                 :create,
+                 :decrement,
+                 :delete,
+                 :expires,
+                 :features,
+                 :fetch,
+                 :increment,
+                 :key?,
+                 :load,
+                 :prefix,
+                 :raw,
+                 :store,
+                 :supports?,
+                 :with)
+
+  def initialize(name, options = {})
+    @storage = Moneta.new(name, options)
+  end
+
+  def cache(key)
+    return storage[key] if storage.key?(key)
+
+    storage[key] = yield
+  end
+
+  protected
+
+  attr_writer :storage
+end
