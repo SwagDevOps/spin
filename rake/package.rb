61a9b16208c1f7a0d93ebd33cb6373bbfb081572
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-12-30 01:06:08 +0100

    package (rake) introduced

--- rake/package.rb
+++ rake/package.rb
@@ -2,16 +2,30 @@
 
 require 'shellwords'
 
-command = ['npx', 'sort-package-json']
-
-handler = lambda do |task, args, cmd = command|
+handler = lambda do |task, command, args = {}|
   env = (args[:environment] || ENV['APP_ENV'] || 'production').to_s
 
-  sh({ 'NODE_ENV' => env }, Shellwords.join(cmd), verbose: false)
+  sh({ 'NODE_ENV' => env }, Shellwords.join(command), verbose: false)
   task.reenable
 end
 
+desc 'Package'
+task :package do
+  [:'package:sort', :'package:install'].each do |t|
+    Rake::Task[t].invoke
+  end
+end
+
 desc 'Sort package.json'
-task :'package:sort', [:environment] do |task, args|
-  handler.call(task, args)
+task :'package:sort' do |task|
+  ['npx', 'sort-package-json'].tap do |command|
+    handler.call(task, command)
+  end
+end
+
+desc 'Install package(s)'
+task :'package:install' do |task|
+  ['yarn', 'install'].tap do |command|
+    handler.call(task, command)
+  end
 end
