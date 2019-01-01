96a992bd6f3d728b2df2697a98ae99827b837737
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-01-01 10:54:11 +0100

    spin/core (spec) examples added

---
+++ spec/spin/core_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# constants ---------------------------------------------------------
+describe Spin::Core, :'spin/core' do
+  it { expect(described_class).to be_const_defined(:Autoloadable) }
+  it { expect(described_class).to be_const_defined(:Cache) }
+  it { expect(described_class).to be_const_defined(:Config) }
+  it { expect(described_class).to be_const_defined(:Container) }
+  it { expect(described_class).to be_const_defined(:Initializer) }
+  it { expect(described_class).to be_const_defined(:Injectable) }
+  it { expect(described_class).to be_const_defined(:Setup) }
+end
