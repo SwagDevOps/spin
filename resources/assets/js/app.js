e953d7f51e076a2fcfe9dab8c13cc264c56620d8
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-01-13 14:24:58 +0100

    js (assets) refactor

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,5 @@
 'use strict'
 
-import 'bootstrap/js/src/alert'
-import 'bootstrap/js/src/button'
+import layout from 'app/layout'
 
-import wow from 'app/wow'
-import imagesReflow from 'app/img_reflow'
-
-imagesReflow()
-wow()
+layout()
---
+++ resources/assets/js/app/layout.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import 'bootstrap/js/src/alert'
+import 'bootstrap/js/src/button'
+
+import wow from './layout/wow'
+import imagesReflow from './layout/img_reflow'
+
+export default function () {
+  imagesReflow()
+  wow()
+}
