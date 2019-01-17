54716b88e0af1e038b8010b136bf92453a27c3c6
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-01-17 08:43:24 +0100

    base/helpers (setup) conditionnal require fix

--- lib/spin/setup/base/helpers.rb
+++ lib/spin/setup/base/helpers.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-# @see https://github.com/hanami/helpers
-require 'hanami/helpers'
-
 # rubocop:disable Style/MixinUsage
 if Gem::Specification.find_all_by_name('hanami-helpers').any?
+  # @see https://github.com/hanami/helpers
+  require 'hanami/helpers'
+
   include(Hanami::Helpers)
 end
 
