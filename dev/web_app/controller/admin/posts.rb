d028404367804497c80be5971f902bf1e5b098e7
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-01-23 18:51:39 +0100

    controller/admin/posts (dev) major changes and improvements

--- dev/web_app/controller/admin/posts.rb
+++ dev/web_app/controller/admin/posts.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-require_relative '../auth'
+require_relative './base'
 
 # Class for posts (admin) management
-class WebApp::Controller::Admin::Posts < WebApp::Controller::Base
-  WebApp::Controller::Admin::BASE_URI.tap do |admin_uri|
-    get(admin_uri.clone.join('posts').to_s) { erb(:welcome) }
-  end
+class WebApp::Controller::Admin::Posts < WebApp::Controller::Admin::Base
+  get('/posts') { erb(:'admin/posts/index') }
 end
