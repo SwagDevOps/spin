4923183ff99a20b690e85b2f05fd0d9001abf993
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-01-24 19:21:42 +0100

    base (lib) documentation added

--- lib/spin/base.rb
+++ lib/spin/base.rb
@@ -7,6 +7,8 @@ require 'sinatra/base'
 #
 # @see https://github.com/sinatra/sinatra/blob/ba63ae84bd52174af03d3933863007ca8a37ac1c/lib/sinatra/base.rb#L904
 class Spin::Base < Sinatra::Base
+  # @api private
+  #
   # @type [Spin::Container]
   @container = nil
 
@@ -16,6 +18,11 @@ class Spin::Base < Sinatra::Base
   class << self
     protected
 
+    # Container acessor.
+    #
+    # Container is set during the ``setup`` phase,
+    # it SHOULD NOT be used for any other purpose.
+    #
     # @return [Spin::Core::Container]
     attr_accessor :container
 
