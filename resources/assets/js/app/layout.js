db96e38b2a8e57101206ef825c8c72df95edc004
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-02-03 20:24:05 +0100

    app/layout (js) md-alerts support introduced

--- resources/assets/js/app/layout.js
+++ resources/assets/js/app/layout.js
@@ -3,6 +3,7 @@
 import $ from 'jquery'
 import imageReflow from './layout/img_reflow'
 import mdl from 'encapsulated-mdl'
+import alert from 'md-alerts'
 
 // expose jQuery to window ------------------------------------------
 if (typeof window !== 'undefined') {
@@ -14,6 +15,7 @@ export default function () {
   $(function () {
     mdl.componentHandler.upgradeDom()
 
+    alert()
     imageReflow()
   })
 }
