b71eaf7760fb0ccfadb3cad5268c9e84d3e1f4df
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-02-07 08:27:04 +0100

    app/layout (js) upgradeDom intrdouced

--- resources/assets/js/app/layout.js
+++ resources/assets/js/app/layout.js
@@ -13,9 +13,27 @@ if (typeof window !== 'undefined') {
 
 export default function () {
   $(function () {
-    mdl.componentHandler.upgradeDom()
-
+    upgradeDom()
     alert()
     imageReflow()
+  })
+}
+
+let upgradeDom = function () {
+  upgradeJsElements()
+
+  mdl.componentHandler.upgradeDom()
+}
+
+/**
+ * @see https://github.com/google/material-design-lite/issues/246
+ */
+let upgradeJsElements = function () {
+  ['textfield', 'button'].forEach(function (type) {
+    $('.mdl-' + type).each(function () {
+      let $element = $(this)
+
+      $element.addClass('mdl-js-' + type)
+    })
   })
 }
