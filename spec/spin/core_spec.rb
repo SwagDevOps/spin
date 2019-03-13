8576a6e588cbf6e3fe1e59077229f221e2d4d8ea
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-03-13 08:34:29 +0100

    core_spec (spec) update

--- spec/spin/core_spec.rb
+++ spec/spin/core_spec.rb
@@ -7,6 +7,7 @@ describe Spin::Core, :'spin/core' do
   it { expect(described_class).to be_const_defined(:Config) }
   it { expect(described_class).to be_const_defined(:Container) }
   it { expect(described_class).to be_const_defined(:Html) }
+  it { expect(described_class).to be_const_defined(:Http) }
   it { expect(described_class).to be_const_defined(:Initializer) }
   it { expect(described_class).to be_const_defined(:Injectable) }
   it { expect(described_class).to be_const_defined(:Setup) }
