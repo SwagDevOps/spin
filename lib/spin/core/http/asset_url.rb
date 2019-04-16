20ae9edff0454064b65001c6474e2d0cf282162b
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-04-16 20:56:32 +0200

    http/asset_url (core) introduced

---
+++ lib/spin/core/http/asset_url.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require_relative './url'
+
+# Represent an asset URL.
+class Spin::Core::Http::AssetUrl < Spin::Core::Http::Url
+  include Spin::Core::Injectable
+
+  inject(:config)
+
+  autoload(:Provider, "#{__dir__}/asset_url/provider")
+
+  def initialize(fragment, **options)
+    options[:config]&.get('assets.hosts').tap do |hosts|
+      if hosts and self.provider.nil?
+        self.provider = Provider.new(hosts)
+      end
+    end
+
+    super(fragment)
+  end
+
+  # Denote request is already set.
+  #
+  # @return [Boolean]
+  def request?
+    !self.request.nil?
+  end
+
+  def request
+    self.provider.to_request || super
+  end
+
+  protected
+
+  attr_accessor(:provider)
+end
---
+++ lib/spin/core/http/asset_url/provider.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative '../asset_url'
+
+# Represent a list of possible (assets serving) hosts.
+#
+# Give a forge request, to replace original request used by ``AssetUrl``
+class Spin::Core::Http::AssetUrl::Provider < Array
+  autoload(:OpenStruct, 'ostruct')
+
+  # @param [Array<String>] hosts
+  def initialize(hosts)
+    hosts.each do |host|
+      self.push(URI.parse(host.to_s))
+    end
+  end
+
+  # Get a struct mimic
+  #
+  # @return [OpenStruct]
+  def to_request
+    OpenStruct.new.tap do |request|
+      self.sample.tap do |host|
+        request.host = host.hostname
+        request.scheme = host.scheme
+        request.port = host.port
+      end
+    end
+  end
+end
