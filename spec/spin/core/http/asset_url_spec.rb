0ef87e40e3e1a796251678f0454f779fe1239b22
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-04-18 09:02:17 +0200

    http/asset_url_spec (core) add examples for request with config

--- spec/spin/core/http/asset_url_spec.rb
+++ spec/spin/core/http/asset_url_spec.rb
@@ -1,6 +1,57 @@
 # frozen_string_literal: true
 
+require 'ostruct'
+require 'uri'
+
 # constants ---------------------------------------------------------
 describe Spin::Core::Http::AssetUrl, :'spin/core/http/asset_url' do
   it { expect(described_class).to be_const_defined(:Provider) }
+end
+
+# instance methods --------------------------------------------------
+describe Spin::Core::Http::AssetUrl, :'spin/core/http/asset_url' do
+  let(:request) do
+    OpenStruct.new(host: 'example.org', scheme: 'http', port: 80)
+  end
+  let(:subject) do
+    Spin::Core::Http::AssetUrl.new('sample/1') do |url|
+      url.request = request
+    end
+  end
+
+  # attributes ------------------------------------------------------
+  it { expect(subject).to respond_to(:path) }
+  it { expect(subject).to respond_to(:fragment) }
+  it { expect(subject).to respond_to(:fragment=) }
+  it { expect(subject).to respond_to(:request) }
+  it { expect(subject).to respond_to(:request=) }
+  it { expect(subject).to respond_to(:request?) }
+  it { expect(subject).to respond_to(:path_only) }
+  it { expect(subject).to respond_to(:path_only=) }
+  it { expect(subject).to respond_to(:query) }
+  it { expect(subject).to respond_to(:query=) }
+  # methods ---------------------------------------------------------
+  it { expect(subject).to respond_to(:path_only?) }
+  it { expect(subject).to respond_to(:to_path) }
+  it { expect(subject).to respond_to(:to_url) }
+  it { expect(subject).to respond_to(:to_uri) }
+  it { expect(subject).to respond_to(:uri) }
+end
+
+# request -----------------------------------------------------------
+describe Spin::Core::Http::AssetUrl, :'spin/core/http/asset_url' do
+  let(:config) do
+    { 'assets.hosts' => ['http://example.org/'] }
+  end
+  let(:subject) do
+    Spin::Core::Http::AssetUrl.new('sample/1', config: config)
+  end
+
+  context '.request?' do
+    it { expect(subject.request?).to eq(true) }
+  end
+
+  context '.request' do
+    it { expect(subject.request).to be_a(OpenStruct) }
+  end
 end
