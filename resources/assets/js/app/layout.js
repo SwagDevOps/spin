eec7e90abb677769e12e89936ed27dfb693ffdf5
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-04-20 17:12:59 +0200

    layout (assets) changes to bulma

--- resources/assets/js/app/layout.js
+++ resources/assets/js/app/layout.js
@@ -2,7 +2,6 @@
 
 import $ from 'jquery'
 import imageReflow from './layout/img_reflow'
-import mdl from 'encapsulated-mdl'
 import alert from 'md-alerts'
 
 // expose jQuery to window ------------------------------------------
@@ -13,27 +12,7 @@ if (typeof window !== 'undefined') {
 
 export default function () {
   $(function () {
-    upgradeDom()
     alert()
     imageReflow()
-  })
-}
-
-let upgradeDom = function () {
-  upgradeJsElements()
-
-  mdl.componentHandler.upgradeDom()
-}
-
-/**
- * @see https://github.com/google/material-design-lite/issues/246
- */
-let upgradeJsElements = function () {
-  ['textfield', 'button'].forEach(function (type) {
-    $('.mdl-' + type).each(function () {
-      let $element = $(this)
-
-      $element.addClass('mdl-js-' + type)
-    })
   })
 }
