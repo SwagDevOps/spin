3f818777f3e01724e7c95808b6b4d9036280df9e
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-04-23 00:38:04 +0200

    base/sessions (setup) syntactic changes

--- lib/spin/setup/base/sessions.rb
+++ lib/spin/setup/base/sessions.rb
@@ -9,6 +9,6 @@ enable(:sessions)
   expire_after: ENV.fetch('SESSION_EXPIRE_AFTER', 2_592_000).to_i,
   secret: ENV.fetch('SESSION_SECRET'),
   old_secret: ENV['SESSION_OLD_SECRET'],
-}.delete_if { |k, v| v.to_s.empty? }.tap do |settings|
+}.delete_if { |_k, v| v.to_s.empty? }.tap do |settings|
   set(:sessions, settings)
 end
