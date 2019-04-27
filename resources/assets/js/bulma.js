f35705a3a5f30e54458befde84a97a5eba6ea374
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-04-27 19:19:01 +0200

    bulma.js (js) introduced

---
+++ resources/assets/js/bulma.js
@@ -0,0 +1,30 @@
+'use strict'
+
+import notificationClickHandler from 'bulma-notifications'
+
+class Bulma {
+  constructor () {
+    this.selectors = {
+      notification_container: '#notifications',
+      notification_delete: '.notification .delete'
+    }
+  }
+
+  /**
+   * @returns {Bulma}
+   *
+   * @see https://bulma.io/documentation/elements/notification/
+   */
+  handleNotifications () {
+    let container = this.selectors.notification_container
+    let trigger = this.selectors.notification_container
+      ? `${this.selectors.notification_container} ${this.selectors.notification_delete}`
+      : this.selectors.notification_delete
+
+    notificationClickHandler(trigger, container)
+
+    return this
+  }
+}
+
+export { Bulma }
