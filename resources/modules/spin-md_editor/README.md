5f3ba17bb3880967283f0d182ddd979c95c174de
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-04-28 19:40:27 +0200

    spin-md_editor (modules) major changes and improvements

---
+++ resources/modules/spin-md_editor/README.md
@@ -0,0 +1,12 @@
+# Use
+
+```scss
+@import '~buefy/dist/buefy.css';
+@import '~spin-md_editor/scss/md_editor';
+```
+
+```js
+import mde from 'spin-md_editor'
+
+mde()
+```
--- resources/modules/spin-md_editor/index.js
+++ resources/modules/spin-md_editor/index.js
@@ -1,19 +1,46 @@
 'use strict'
 
 import Vue from 'vue'
+import Tabs from 'buefy/dist/components/tabs'
+
+import cuid from 'cuid'
 import * as marked from 'marked'
 import * as _ from 'lodash'
 
-const vue = function (c) {
+/**
+ * Class used to recognize markdown editors.
+ *
+ * @type {string}
+ */
+const cname = 'is-md_editor'
+
+/**
+ * @private
+ *
+ * @param {object} c
+ * @returns {Vue | CombinedVueInstance<Vue, object, object, object, Record<never, any>>}
+ */
+const _vue = function (c) {
+  Vue.use(Tabs)
+
   let vue = (c) => new Vue(c)
 
-  window.addEventListener('load', () => vue(c))
+  return vue(c)
 }
 
-export default function () {
-  vue({
-    el: '#editor',
-    data: { input: '' },
+/**
+ * Install an individual editor.
+ *
+ * @param {String} id
+ */
+const installEditor = function (id) {
+  _vue({
+    el: `#${id}`,
+    data: {
+      input: '',
+      activeTab: 0,
+      showBooks: false
+    },
     mounted () {
       this.input = this.$refs.value.dataset.value
     },
@@ -28,4 +55,27 @@ export default function () {
       }, 300)
     }
   })
+}
+
+/**
+ * Install editors for given class name ``cname``.
+ *
+ * @param {String} cname
+ */
+const installEditors = function (cname) {
+  Array.from(document.getElementsByClassName(cname)).forEach(function (element) {
+    let id = `${cname}_${cuid()}`
+
+    element.setAttribute('id', id)
+
+    installEditor(id)
+  })
+}
+
+// exports ----------------------------------------------------------
+
+export { installEditors, installEditor }
+
+export default function () {
+  window.addEventListener('DOMContentLoaded', () => installEditors(cname))
 }
