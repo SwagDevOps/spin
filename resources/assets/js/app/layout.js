cbd0481dbb58df5affc62a8d29afc9450789de9c
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-05-07 08:54:53 +0200

    app/layout (js) use spin-img_reflow

--- resources/assets/js/app/layout.js
+++ resources/assets/js/app/layout.js
@@ -1,14 +1,7 @@
 'use strict'
 
-import $ from 'jquery'
-import imageReflow from './layout/img_reflow'
+import imageReflow from 'spin-img_reflow'
 import { Bulma } from '../bulma'
-
-// expose jQuery to window ------------------------------------------
-if (typeof window !== 'undefined') {
-  window.jQuery = $
-  window.$ = $
-}
 
 const bulmaInstall = function () {
   let bulma = new Bulma()
