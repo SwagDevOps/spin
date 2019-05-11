dad130f29002e2f36b9229a4f700254cfcca29cf
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-05-11 20:26:47 +0200

    app (js) use router

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,14 @@
 'use strict'
 
+import Navigo from 'navigo'
 import layout from './app/layout'
 import mde from 'spin-md_editor'
 
-layout()
-mde()
+const router = new Navigo(null, false, '#')
+
+router
+  .on(() => layout())
+  .on({
+    '/pages/editor': () => mde()
+  })
+  .resolve()
