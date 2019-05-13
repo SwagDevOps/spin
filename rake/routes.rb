4dc16c98ffcb6e29eca51f04c9d5cda1200c62fd
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-05-13 09:32:49 +0200

    routes (rake) minor changes and improvements

--- rake/routes.rb
+++ rake/routes.rb
@@ -23,9 +23,9 @@ base_routing = lambda do |c, base = '/'|
 
             routes[uri] = {
               URI: Pathname.new(base.to_s).join(uri).to_s,
-              controller: c,
-              path: base,
               methods: [method],
+              path: base,
+              controller: c,
             }.merge(routes[uri] || {})
 
             routes[uri][:methods].push(method).uniq!
@@ -38,25 +38,31 @@ base_routing = lambda do |c, base = '/'|
 end
 
 routing = lambda do
-  app.call.routing.map do |base, c|
-    base_routing.call(c, base).values.map do |v|
-      v.tap do
-        v[:methods] = v[:methods].map(&:to_s).join(', ')
-      end
-    end
-  end.flatten
+  app.call.routing.map { |base, c| base_routing.call(c, base).values }.flatten
 end
 
 routing_task = lambda do
   require 'tty-table'
 
-  routing.call.tap do |r|
-    [r[0] ? r[0].keys.map { |t| t.to_s.sub(/\w/, &:capitalize) } : [],
-     r.map(&:values)].tap do |v|
-      TTY::Table.new(v[0].to_a, v[1]).tap do |table|
-        table.render(:ascii) do |renderer|
-          renderer.padding = [0, 1, 0, 1]
-        end.tap { |t| $stdout.puts(t) }
+  routing.call.tap do |routes|
+    unless routes.empty?
+      routes.map! do |row|
+        row.tap do
+          # @formatter:off
+          {
+            methods: row[:methods].join(', '),
+            controller: row[:controller].name.gsub(/^#{app.call.name}::/, '')
+          }.tap { |o| row.merge!(o) }
+          # @formatter:on
+        end
+      end
+
+      routes[0].keys.map { |t| t.to_s.sub(/\w/, &:capitalize) }.tap do |headers|
+        TTY::Table.new(headers, routes.map(&:values)).tap do |table|
+          table.render(:ascii) do |renderer|
+            renderer.padding = [0, 1, 0, 1]
+          end.tap { |t| $stdout.puts(t) }
+        end
       end
     end
   end
