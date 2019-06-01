d1263f73aaaa9a9f7d7083619e8fcf4596192e34
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-06-02 00:09:29 +0200

    view_eval_helper (helpers) use a "storage" on instance instead of serialization

--- lib/spin/helpers/view_eval_helper.rb
+++ lib/spin/helpers/view_eval_helper.rb
@@ -4,6 +4,7 @@ require_relative '../helpers'
 
 # Erb include helper
 module Spin::Helpers::ViewEvalHelper
+  autoload(:Digest, 'digest')
   autoload(:Pathname, 'pathname')
 
   protected
@@ -18,7 +19,7 @@ module Spin::Helpers::ViewEvalHelper
   #
   # @return [Hanami::Helpers::HtmlHelper::HtmlBuilder]
   def view_eval(path, variables = {})
-    view_eval_vars(variables).tap do |lines|
+    view_eval_vars(path, variables).tap do |lines|
       Pathname.new(self.settings.views).join("#{path}.rb").tap do |inc|
         lines.clone.push(inc.read).join("\n").tap do |content|
           return self.instance_eval(content, inc.to_s, 1 - lines.size)
@@ -27,15 +28,29 @@ module Spin::Helpers::ViewEvalHelper
     end
   end
 
+  # @param [String] path
+  # @param [Hash] variables
+  #
+  # @return [String]
+  def view_eval_store(path, variables = {})
+    Digest::SHA256.hexdigest(path.to_s).tap do |key|
+      @view_eval_store ||= {}
+      @view_eval_store[key] = variables.clone.freeze
+    end
+  end
+
+  # @param [String] path
   # @param [Hash] variables
   #
   # @return [Array<String>]
-  def view_eval_vars(variables = {})
+  def view_eval_vars(path, variables = {})
     return [] if variables.empty?
 
     ['# frozen_string_literal: true'].tap do |lines|
-      variables.each do |k, v|
-        lines.push("#{k} = Marshal.load(#{Marshal.dump(v).inspect})")
+      view_eval_store(path, variables).tap do |key|
+        variables.each_key do |k|
+          lines.push(" #{k}= @view_eval_store[#{key.inspect}][#{k.inspect}]")
+        end
       end
     end
   end
