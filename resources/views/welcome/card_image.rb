445663a7f814979b416ff27b0b319260f9d20583
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-06-03 08:40:39 +0200

    welcome (views) fix apsect-ratio

--- resources/views/welcome/card_image.rb
+++ resources/views/welcome/card_image.rb
@@ -4,5 +4,5 @@ size = image_size(image)
 html.div(class: 'card-image', 'data-updated_at': Time.now) do
   tag('progressive-img',
       src: asset_url(image),
-      'aspect-ratio': (size.height * 1.0) / size.width)
+      ':aspect-ratio': (size.height * 1.0) / size.width)
 end
